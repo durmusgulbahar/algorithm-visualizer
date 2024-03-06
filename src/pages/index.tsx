59568627fe3345a7fdcf2c265b9b8eb5ada4d0ab
durmusgulbahar <durmus.gulbahar@outlook.com>
2024-03-06 13:56:52 +0300

    change design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import InsertionSortVisualizer from "../components/InsertionSort";
 
 export default function Home() {
+
   return (
     <main>
-      <InsertionSortVisualizer arr={[3,1,4,4,2]}/>
+      <InsertionSortVisualizer arr={[3,1,4,4,2,10,21,89]}/>
+      <input name="myInput" />
+
     </main>
   );
 }
